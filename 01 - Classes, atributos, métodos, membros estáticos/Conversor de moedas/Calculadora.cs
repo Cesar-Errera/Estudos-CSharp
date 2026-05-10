eff1b1c0385181e2b1008365c5ced65743891da8
Cesar-Errera <cesarerrera@hotmail.com>
2026-05-10 19:09:29 -0300

    Add files via upload

---
+++ 01 - Classes, atributos, métodos, membros estáticos/Conversor de moedas/Calculadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversor_de_moedas
+{
+    internal class Calculadora
+    {
+        public static double Iof = 6.0;
+        
+        public static double ValorAPagar(double cotacao, double compra)
+        {
+            double total = compra * cotacao;
+            return total + total * Iof / 100;
+        }
+    }
+}
---
+++ 01 - Classes, atributos, métodos, membros estáticos/Conversor de moedas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Conversor_de_moedas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Qual é a cotação do dolar atual?  ");
+            double cotacao = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double compra = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double resultado = Calculadora.ValorAPagar(cotacao,compra);
+
+            Console.WriteLine("Valor a ser pago em reais = " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
