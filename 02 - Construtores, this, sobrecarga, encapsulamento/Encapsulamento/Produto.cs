0313432d70a6434dac7fac8da2d329414f271e7b
Cesar-Errera <cesarerrera@hotmail.com>
2026-05-13 19:31:18 -0300

    Add files via upload

---
+++ 02 - Construtores, this, sobrecarga, encapsulamento/Encapsulamento/Produto.cs
@@ -0,0 +1,86 @@
+﻿using System.Globalization;
+
+namespace Encapsulamento
+{
+    internal class Produto
+    {
+        // no privado usamos por padrão _XXX
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        // ---------------------------------------------------------------
+
+        //Construtores
+        public Produto()
+        {
+
+        }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        // ------------------------------------------------------------------
+
+        // Encapsulamento
+
+        //usando Get() - nome
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        //usando Set() - nome
+        public void SetNome(string nome)
+        {
+            if (nome != null && nome.Length > 1) // impede nome nulo e menor que 1 caracter
+            {
+                _nome = nome;
+            } 
+        }
+
+        public double GetPreco()
+        {
+            return _preco;
+        }
+
+        public int GetQuantidade()
+        {
+            return _quantidade;
+        }
+
+        // ------------------------------------------------------------------------------
+
+        // Métodos
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return _nome
+                + ", $ "
+                + _preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + _quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque();
+        }
+
+    }
+}
---
+++ 02 - Construtores, this, sobrecarga, encapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // todo atributo é definido como private
+            // implementa-se metodos Get e Set para
+            // cada atributo, conforme regras do negocio
+
+            Produto p = new Produto("TV",500,10);
+
+            // devemos impedir que o usuario faça isso
+            // p.Quantidade = -10;
+
+            // Como mudariamos o nome?
+            // p._nome = "TV 4K"
+            // Assim não funcionaria pois a variavel na classe é private
+
+            p.SetNome("TV 4K");
+
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetPreco());
+            Console.WriteLine(p.GetQuantidade());
+
+        }
+    }
+}
